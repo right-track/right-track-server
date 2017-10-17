bbe0038b7d1f6acdcea628146dde3603adc7693c
David Waring <dev@davidwaring.net>
2017-10-17 17:51:16 -0400

    Add mysql schema

---
+++ rt_api.sql
@@ -0,0 +1,156 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.10)
+# Database: rt_api
+# Generation Time: 2017-10-17 21:25:50 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table apps
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `apps`;
+
+CREATE TABLE `apps` (
+  `agency` varchar(6) NOT NULL DEFAULT '',
+  `name` varchar(48) DEFAULT NULL,
+  `version` varchar(6) NOT NULL DEFAULT '',
+  `notes` text NOT NULL,
+  `url` varchar(72) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table clients
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `clients`;
+
+CREATE TABLE `clients` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user` text NOT NULL,
+  `email` text NOT NULL,
+  `client_name` varchar(64) NOT NULL DEFAULT '',
+  `client_id` varchar(64) NOT NULL DEFAULT '',
+  `client_key` varchar(64) NOT NULL DEFAULT '',
+  `access` set('auth','registration','favorites','transit','gtfs','stations','trips','updates','debug','admin') NOT NULL DEFAULT '',
+  `session_length_max` int(11) DEFAULT '30',
+  `session_length_inactive` int(11) DEFAULT '7',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table favorites
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `favorites`;
+
+CREATE TABLE `favorites` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `agency` varchar(8) NOT NULL DEFAULT '',
+  `type` int(11) unsigned NOT NULL,
+  `sequence` int(11) unsigned NOT NULL,
+  `parameters` longtext,
+  `options` longtext,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table favorites_modified
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `favorites_modified`;
+
+CREATE TABLE `favorites_modified` (
+  `user_id` int(11) NOT NULL,
+  `mnr` datetime DEFAULT NULL,
+  `lirr` datetime DEFAULT NULL,
+  `njt` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table messages
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `agency` varchar(8) NOT NULL DEFAULT '',
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `title` varchar(36) NOT NULL DEFAULT '',
+  `body` text NOT NULL,
+  `link_title` varchar(36) DEFAULT NULL,
+  `link_url` varchar(72) DEFAULT NULL,
+  `timestamp` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` varchar(64) NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `accessed` datetime NOT NULL,
+  `inactive` datetime NOT NULL,
+  `expires` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` varchar(64) NOT NULL DEFAULT '',
+  `username` varchar(64) DEFAULT '',
+  `email` varchar(254) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `salt` varchar(64) NOT NULL DEFAULT '',
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `user_modified` datetime NOT NULL,
+  `password_modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
